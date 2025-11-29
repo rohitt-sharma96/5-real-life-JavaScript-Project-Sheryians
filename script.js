fb6d41db1c88a46eaafa82c4bb9f75c031bc5300
Rohit Sharma <sharmarohit469494@gmail.com>
2025-11-29 23:01:56 +0530

    Project-2 Insta Like Button

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
+let container = document.querySelector('#container');
 
-let add = document.getElementById('add')
+let img = document.querySelector('img')
 
-let h5 = document.querySelector('h5')
+let like = document.querySelector('i');
 
 
-let flag = false;
 
-add.addEventListener('click',()=>{
-    if(flag === false){
-        h5.innerHTML = "Friends"
-        h5.style.color = 'green';
-        add.innerHTML = "- remove"
-        flag = true;
-    } 
-    else{
-        h5.innerHTML = "Stranger"
-        h5.style.color = "red"
-        add.innerHTML = "+ add Friend"
-        flag = false
+container.addEventListener('dblclick', () => {
 
-    }
+    like.style.transform = 'translate(-50%, -50% ) scale(1)'
+    like.style.opacity = 0.8;
+    like.style.color = 'red';
+    
+
+    setTimeout(function () {
+        like.style.opacity = 0;
+    }, 1500)
+
+    setTimeout(function () {
+        like.style.transform = 'translate(-50%, -50% ) scale(0)'
+
+    }, 3000)
+
+
 })
 
