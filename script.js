0f6bfb2778a274d81921f8d02e18bfe7a27adb35
Rohit Sharma <sharmarohit469494@gmail.com>
2025-11-30 11:44:08 +0530

    Project-3 Custom Cursor

--- script.js
+++ script.js
@@ -1,27 +1,9 @@
-let container = document.querySelector('#container');
-
-let img = document.querySelector('img')
-
-let like = document.querySelector('i');
+let main = document.querySelector('.main')
+let crsr = document.querySelector('.cursor')
 
 
-
-container.addEventListener('dblclick', () => {
-
-    like.style.transform = 'translate(-50%, -50% ) scale(1)'
-    like.style.opacity = 0.8;
-    like.style.color = 'red';
+main.addEventListener('mousemove', (dets)=>{
     
-
-    setTimeout(function () {
-        like.style.opacity = 0;
-    }, 1500)
-
-    setTimeout(function () {
-        like.style.transform = 'translate(-50%, -50% ) scale(0)'
-
-    }, 3000)
-
-
+   crsr.style.left = dets.x+"px";
+   crsr.style.top = dets.y+"px";
 })
-
