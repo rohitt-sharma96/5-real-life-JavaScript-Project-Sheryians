249a92141c9c9a598a5a5edb4fa620bf6155a2e2
Rohit Sharma <sharmarohit469494@gmail.com>
2025-12-01 17:51:15 +0530

    Project-3 Multiple Image Hovering Animation

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-let main = document.querySelector('.main')
-let crsr = document.querySelector('.cursor')
+let elem = document.querySelectorAll('.elem');
 
 
-main.addEventListener('mousemove', (dets)=>{
-    
-   crsr.style.left = dets.x+"px";
-   crsr.style.top = dets.y+"px";
+
+let img = document.querySelector('#elem1 img');
+
+
+
+
+elem.forEach(function (val) {
+
+   // console.log(val.children)- dekne ke liye val ke andar children mein kya kya mil rha hai
+   
+   val.addEventListener('mousemove', (dets)=>{
+      // val.children[1].style.top = dets.y+"px"
+      val.children[1].style.left = dets.x+"px"
+   })
+
+   val.addEventListener('mouseenter', () => {
+      
+      val.children[1].style.opacity = 1;
+   })
+
+   val.addEventListener('mouseleave', () => {
+      
+      val.children[1].style.opacity = 0;
+
+
+
+   })
 })
