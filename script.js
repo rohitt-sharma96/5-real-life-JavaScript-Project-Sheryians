7ae389749dfd6f66d6574b55583292b1256a7cf6
Rohit Sharma <sharmarohit469494@gmail.com>
2025-12-01 19:07:55 +0530

    Project-5 Insta Story Feature

--- script.js
+++ script.js
@@ -1,31 +1,45 @@
-let elem = document.querySelectorAll('.elem');
+let arr = [
+   {
+      dp: "https://images.unsplash.com/photo-1529139574466-a303027c1d8b?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      story: "https://images.unsplash.com/photo-1676576625429-e00537557449?q=80&w=715&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+   },
+   {
+      dp: "https://plus.unsplash.com/premium_photo-1682095757120-c9abb908ed60?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      story: "https://plus.unsplash.com/premium_photo-1682095763838-0ca2593193bd?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+   },
+   {
+      dp: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      story: "https://images.unsplash.com/photo-1626858707111-2fefbc9a848e?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+   },
+   {
+      dp: "https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      story: "https://images.unsplash.com/photo-1566812086215-c29c82c4cf93?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+   }
+]
 
 
+let clutter = "";
 
-let img = document.querySelector('#elem1 img');
+arr.forEach((elem, idx) => {
+   clutter += `
+               <div class="story">
+                <img id="${idx}" src="${elem.dp}" alt="">
+               </div>
+              ` 
+})
 
+let stories = document.querySelector('.story-container')
 
+stories.innerHTML = clutter;
 
-
-elem.forEach(function (val) {
-
-   // console.log(val.children)- dekne ke liye val ke andar children mein kya kya mil rha hai
+stories.addEventListener('click', (dets)=>{
+//   let GoldenValue = arr[dets.target.id].story
+   document.querySelector('#full-screen').style.display = 'block';
+   document.querySelector('#full-screen').style.backgroundImage = `url(${arr[dets.target.id].story})`
    
-   val.addEventListener('mousemove', (dets)=>{
-      // val.children[1].style.top = dets.y+"px"
-      val.children[1].style.left = dets.x+"px"
-   })
 
-   val.addEventListener('mouseenter', () => {
-      
-      val.children[1].style.opacity = 1;
-   })
+   setTimeout(()=>{
+   document.querySelector('#full-screen').style.display = 'none';
 
-   val.addEventListener('mouseleave', () => {
-      
-      val.children[1].style.opacity = 0;
-
-
-
-   })
+   },5000)
 })
